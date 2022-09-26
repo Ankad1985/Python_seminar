5131ad733598d860cbfe33792f4d38f8472cd495
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-09-26 15:33:58 +0300

    Вывод чисел от -N до N

---
+++ Task003_From_-N_to_N.py
@@ -0,0 +1,16 @@
+# 3. Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N
+
+a = int(input('Введите число: '))
+
+for i in range(-a, a+1):
+    print(i,end=' ')
+    
+# num = int(input())
+#neg_num = -num
+
+# while num != neg_num:
+# print(neg_num, end=", ")
+# if num > 0:
+# neg_num += 1
+# else:
+# neg_num -= 1
